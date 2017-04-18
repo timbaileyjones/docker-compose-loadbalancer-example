cf9b4891c266320276a67936f5f1198a5b87fbff
Tim Jones <tim@timjones.com>
2017-04-17 22:59:25 -0400

    flask implementation of data ingestion complete

--- flask-site/flask-site.py
+++ flask-site/flask-site.py
@@ -1,6 +1,6 @@
 import datetime
 import os
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 
 hostname = os.uname()[1]
@@ -9,29 +9,17 @@ app = Flask(__name__, static_url_path='/static')
 api = Api(app)
 
 
-class Schedule(Resource):
-    def get(self):
-        logfile = open('/tmp/shared_logs/%s_access.log' % hostname, 'a');
-        print "logfile", logfile
-        logfile.write('%s schedule sent\n' % datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-        logfile.close()
-        return [
-        {
-                'date': '04/28/2017 19:00:00',
-                'venue': 'Tampa Covenant Church',
-                'desc': 'Spring Concert 2017'
-        },
-        {
-                'date': '07/03/2017 19:00:00',
-                'venue': 'Osborne Riverfront Park',
-                'desc': 'Summer Concert'
-        },
-        ]
-
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
-api.add_resource(Schedule, '/schedule')
+
+@app.route('/api/data', methods = ['POST'])
+def postData():
+    payload = str(request.data)
+    logfile = open('/tmp/shared_logs/%s_access.log' % hostname, 'a');
+    logfile.write('%s accepted input: %s\n' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), payload))
+    logfile.close()
+    return "{'status': 'success'}"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
