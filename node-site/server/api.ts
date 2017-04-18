d8e0c3e45157d74e634c237e781812793e037809
Tim Jones <tim@timjones.com>
2017-04-18 09:57:08 -0400

    finished /api/data on node-site

--- node-site/server/api.ts
+++ node-site/server/api.ts
@@ -1,13 +1,19 @@
 import express = require("express");
 import log4js = require("log4js");
+import moment = require("moment");
 
 let log = log4js.getLogger();
+let fs = require('fs');
+let os = require('os');
+let logfile = `/tmp/shared_logs/` + os.hostname() + `_access.node.log`;
 
 const router = express.Router();
 export = router;
 
-/* GET api listing. */
-router.get("/", (req, res) => {
-	log.info("test api");
-	res.send("api works");
+/* POST data listing. */
+router.post("/data", (req, res) => {
+	console.log('req.body', req.body);
+	let line = moment().format("YYYY-MM-DD hh:mm:ss") + " " + JSON.stringify(req.body) + "\n";
+    fs.appendFileSync(logfile, line);
+	res.send({ "status": "success"}); 
 });
