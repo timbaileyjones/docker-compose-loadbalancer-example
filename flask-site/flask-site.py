e6cf610e50d1242587706be4572f96932dc2519c
Tim Jones <tim@timjones.com>
2017-04-18 14:30:26 -0400

    turned debugging off in flask site.

--- flask-site/flask-site.py
+++ flask-site/flask-site.py
@@ -22,4 +22,4 @@ def postData():
     return "{'status': 'success'}"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=False)
